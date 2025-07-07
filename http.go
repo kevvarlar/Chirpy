e49625bf19b704317c4f9d9e8b469d1b154425dd
Kevin <kevinvar2805@gmail.com>
2025-07-07 17:55:26 -0500

    Added a metrics for hits and a way to reset the hits

--- http.go
+++ http.go
@@ -1,8 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
 
-func readiness(res http.ResponseWriter, req *http.Request) {
+)
+
+func readiness(res http.ResponseWriter, _ *http.Request) {
+	res.WriteHeader(200)
+	res.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metrics(res http.ResponseWriter, _ *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	stringHits := strconv.FormatInt(int64(hits), 10)
+	res.WriteHeader(200)
+	res.Write([]byte("Hits: " + stringHits))
+}
+
+func (cfg *apiConfig) reset(res http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
 	res.WriteHeader(200)
 	res.Write([]byte("OK"))
 }
--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	ServeMux := http.NewServeMux()
-	ServeMux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	ServeMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+
 	ServeMux.HandleFunc("/healthz", readiness)
+	ServeMux.HandleFunc("/metrics", apiCfg.metrics)
+	ServeMux.HandleFunc("/reset", apiCfg.reset)
 	server := &http.Server{
 		Handler: ServeMux,
 		Addr: ":8080",
