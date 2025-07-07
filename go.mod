3b46f5bcb4eba499be50928afe5db140383353bd
Kevin <kevinvar2805@gmail.com>
2025-07-07 14:11:25 -0500

    Started http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kevvarlar/Chirpy
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	ServeMux := http.NewServeMux()
+	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	ServeMux.HandleFunc("/healthz", readiness)
+	server := &http.Server{
+		Handler: ServeMux,
+		Addr: ":8080",
+	}
+	fmt.Println("Server running on http://localhost" + server.Addr)
+	log.Fatal(server.ListenAndServe())
+}
